c10ac8bb2747f58dd6720c0cb8c03c889673d894
Kenneth Hedahl <kh@hedahl.biz>
2015-01-26 11:21:24 +0100

    Updated DB settings

--- includes/config.php
+++ includes/config.php
@@ -9,6 +9,6 @@ class Config {
     /** MySQL settings */
     public static $mysql_host = "localhost";
     public static $mysql_user = "layertask";
-    public static $mysql_pass = "Sist1hg2!";
+    public static $mysql_pass = "somepw";
     public static $mysql_db = "layertask";
 }
