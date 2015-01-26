9321edf6ee6cbbdf4d9e54d60094648f9e718af8
Kenneth Hedahl <kh@hedahl.biz>
2015-01-26 14:12:17 +0100

    Added help-page

---
+++ content/help.php
@@ -0,0 +1,30 @@
+<?php
+$setup = array();
+$setup['title'] = "Help";
+$setup['name'] = "Help";
+$setup['description'] = "Learn how to use LayerTask";
+Design::header($setup);
+?>
+<?php
+$sql = "SELECT * FROM " . Config::$db_help;
+$db = $con->query($sql);
+
+
+while ($result = $db->fetch_object()) {
+    echo '<div class="panel panel-default panel-faq">
+    <div class="panel-heading">
+        <a data-toggle="collapse" data-parent="#accordion-cat-18" href="#faq-cat-18-sub-18">
+            <h4 class="panel-title">
+                ' . $result->title . ' <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
+            </h4>
+        </a>
+    </div>
+    <div id="faq-cat-18-sub-18" class="panel-collapse collapse">
+        <div class="panel-body">
+            ' . $result->content . ' </div>
+    </div>
+</div>';
+}
+?>
+<?php
+Design::footer();
